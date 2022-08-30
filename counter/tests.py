e5cab8c822134fcdc901cf08be20ad73e66f82f1
Marcel B. Daryanto <marcel.budiono@gmail.com>
2022-08-30 14:30:32 +0700

    adding test case

--- counter/tests.py
+++ counter/tests.py
@@ -1,12 +1,20 @@
+import time
 from django.test import TestCase, Client
 
+
 class CounterTestCast(TestCase):
-    def test_user_register_login(self):
+    def test_counter_increment(self):
         c = Client()
+        # As a non-logged in user, I can see the total number of clicks
         response = c.get('/counter', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertGreaterEqual(response.json()['counter'], 0)
 
+        # As a non-logged in user, I cannot access increment
+        response = c.post('/increment', follow=True)
+        self.assertNotEqual(response.status_code, 200)
+
+        # As a user I can register an account on the Button Press website
         response = c.post('/members/register_user', { 'username': 'randomusername', 'password1': 'supersimplepassword', 'password2': 'supersimplepassword'}, follow=True)
         self.assertEqual(response.status_code, 200)
 
@@ -14,6 +22,7 @@ class CounterTestCast(TestCase):
         self.assertEqual(response.status_code, 200)
         counter = response.json()['counter']
 
+        # successfully increment 5 times consecutively
         for _ in range(5):
             response = c.post('/increment', follow=True)
             self.assertEqual(response.status_code, 200)
@@ -25,6 +34,12 @@ class CounterTestCast(TestCase):
         # after increment 5 times consecutively (less than 60 secs = assumption)
         response = c.post('/increment', follow=True)
         self.assertNotEqual(response.status_code, 200)
+
+        time.sleep(60)
+
+        # after 60 seconds user allowed to increment
+        response = c.post('/increment', follow=True)
+        self.assertEqual(response.status_code, 200)
 
 
 
--- members/tests.py
+++ members/tests.py
@@ -5,11 +5,14 @@ from django.urls import reverse
 class MemberTestCast(TestCase):
     def test_user_register_login(self):
         c = Client()
+        # As a user I can register an account on the Button Press website
         response = c.post('/members/register_user', { 'username': 'randomusername', 'password1': 'supersimplepassword', 'password2': 'supersimplepassword'}, follow=True)
         self.assertEqual(response.status_code, 200)
 
+        # As a user I can log out of the Button Press website
         response = c.get('/members/logout_user', follow=True)
         self.assertEqual(response.status_code, 200)
 
+        # As a user I can login to the Button Press website
         response = c.post('/members/login_user', { 'username': 'randomusername', 'password': 'supersimplepassword'}, follow=True)
         self.assertEqual(response.status_code, 200)
